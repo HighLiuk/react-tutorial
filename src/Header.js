f1cc75b54d7d07c337c46dd482695ecca4d53050
Luca Di Fazio <highliuk@gmail.com>
2021-10-10 01:17:35 +0200

    Styling Components with CSS

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,7 @@
-import PropTypes from "prop-types"
-
-function Header({ title }) {
+export default function Header({ title }) {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={{ color: "red", backgroundColor: "black" }}>{title}</h1>
     </header>
   )
 }
-
-// default values for props
-Header.defaultProps = {
-  title: "Task Tracker",
-}
-
-// statically typing for props
-Header.propTypes = {
-  title: PropTypes.string,
-  // you can also force a prop to be required by writing
-  // title: PropTypes.string.isRequired,
-}
-
-export default Header
