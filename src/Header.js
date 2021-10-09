c01fc58e92696b9b4c9cfba51b04c7a51c4e6d3f
Luca Di Fazio <highliuk@gmail.com>
2021-10-10 01:13:33 +0200

    Using PropTypes to work with props in a robust way

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,23 @@
-export default function Header({ title }) {
+import PropTypes from "prop-types"
+
+function Header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
     </header>
   )
 }
+
+// default values for props
+Header.defaultProps = {
+  title: "Task Tracker",
+}
+
+// statically typing for props
+Header.propTypes = {
+  title: PropTypes.string,
+  // you can also force a prop to be required by writing
+  // title: PropTypes.string.isRequired,
+}
+
+export default Header
