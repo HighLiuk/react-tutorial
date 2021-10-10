c45bbb305f8fd9fdf3745250b57db32e6db62f60
Luca Di Fazio <highliuk@gmail.com>
2021-10-10 16:25:55 +0200

    Added Delete Task

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Tasks from "./Tasks"
 export default function App() {
   const [tasks, setTasks] = useState(initialTasks)
 
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
