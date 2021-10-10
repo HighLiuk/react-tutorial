6239eca8c55be2bb7c157e5c63042826e5f7f0bb
Luca Di Fazio <highliuk@gmail.com>
2021-10-10 16:27:43 +0200

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ export default function App() {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   )
 }
