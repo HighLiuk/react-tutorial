fe0bde8f4aab7531cfb4f7d33433a042954e8bd5
Luca Di Fazio <highliuk@gmail.com>
2021-10-10 17:46:13 +0200

    Added Json Server

--- src/App.js
+++ src/App.js
@@ -50,23 +50,4 @@ export default function App() {
   )
 }
 
-const initialTasks = [
-  {
-    id: 1,
-    text: "Doctor's Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "Feb 5th at 2:30pm",
-    reminder: false,
-  },
-]
+const initialTasks = []
